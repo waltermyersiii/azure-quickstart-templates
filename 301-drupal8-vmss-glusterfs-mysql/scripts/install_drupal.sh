aa0181ed516932f46b7703061687c3ea005b7f79
Walter Myers III <walterm@microsoft.com>
2020-10-10 12:51:20 -0700

    Update install_drupal.sh
    Updated error message to show supposedly missing params.

--- 301-drupal8-vmss-glusterfs-mysql/scripts/install_drupal.sh
+++ 301-drupal8-vmss-glusterfs-mysql/scripts/install_drupal.sh
@@ -135,6 +135,7 @@ done
 if [ "$GLUSTER_FIRST_NODE_NAME" == "" ] || [ "$GLUSTER_VOLUME_NAME" == "" ] || [ "$MYSQL_PASSWORD" == "" ];
 then
     log "Script executed without required parameters"
+    echo "Parameters: $GLUSTER_FIRST_NODE_NAME, $GLUSTER_VOLUME_NAME, $MYSQL_PASSWORD"
     echo "You must provide all required parameters." >&2
     exit 3
 fi
